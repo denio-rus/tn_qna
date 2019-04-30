036e7285b2a8a0d88dfe27d6af6aa630baa3c35a
denio-rus <denio@yandex.ru>
2019-05-01 00:00:13 +0700

    Add tests for Answers#new

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'GET #new' do
+    let(:question) { create(:question) }
+    it 'assigns the question that answers to @question' do
+      get :new, params: { question_id: question } 
+      expect(assigns(:question)).to eq question
+    end
+    it 'assigns a new answer for @question to @answer' do
+      get :new, params: { question_id: question }
+      expect(assign(:answer)).to be_a_new(:question.answers)
+    end
 
+    it 'renders new view' do
+      get :new, params: { question_id: question }
+      expect(response).to render_templete :new
+    end
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :answer do
     body { "MyText" }
     question { "" }
+
+  trait :invalid
+    body { nil }
   end
 end
