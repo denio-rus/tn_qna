005f3912cb2ce07ebcaa817631251826df894638
denio-rus <denio@yandex.ru>
2019-05-01 03:06:47 +0700

    Remove before_action for load question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,4 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:edit, :show, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -7,9 +6,7 @@ class QuestionsController < ApplicationController
 
   def show; end
 
-  def new 
-    @question = Question.new
-  end
+  def new; end
 
   def edit; end
 
@@ -23,7 +20,7 @@ class QuestionsController < ApplicationController
   end
 
   def update 
-    if @question.update(question_params)
+    if question.update(question_params)
       redirect_to @question
     else 
       render :edit
@@ -31,15 +28,17 @@ class QuestionsController < ApplicationController
   end
 
   def destroy 
-    @question.destroy
+    question.destroy
     redirect_to questions_path
   end
   
   private
 
-  def load_question
-    @question = Question.find(params[:id])
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
   end
+
+  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
