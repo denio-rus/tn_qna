735163bd0da503c195ef64ac32a002d388756e9f
denio-rus <denio@yandex.ru>
2019-05-01 03:24:07 +0700

    refact

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,15 +1,19 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: [:new, :create]
+  
   def new 
-    @question = Question.find(params[:question_id])
     @answer = @question.answers.new
   end
 
   def create
-    @question = Question.find(params[:question_id])
     @question.answers.create(answer_params)
   end
 
   private
+  
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body)
