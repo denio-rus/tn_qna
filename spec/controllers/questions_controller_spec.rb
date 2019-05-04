c391531f99da7c044535ab510ecc76a44581b705
denio-rus <denio@yandex.ru>
2019-05-04 20:57:46 +0700

    Questions List

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #index' do 
     let(:questions) { create_list(:question, 3) }
+    
     before { get :index }
+
     it 'populates an array of all questions' do 
       expect(assigns(:questions)).to match_array(questions)
     end
---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can view the list of all questions', %q{ 
+  In order to find interesting question
+  As an any user
+  I'd like to be able to see the list of all questions
+} do
+  scenario 'User views questions' do 
+    Question.create(title: "question 1", body: "text")
+    Question.create(title: "question 2", body: "text")
+
+    visit questions_path
+    
+    expect(page).to have_content "question 1"
+    expect(page).to have_content "question 2"
+  end
+end
