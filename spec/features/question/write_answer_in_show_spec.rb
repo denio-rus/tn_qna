b99e436388fa5356cd8e367cb665278cd270d41a
denio-rus <denio@yandex.ru>
2019-05-04 23:38:02 +0700

    Write your answer in the page showing the question

---
+++ spec/features/question/write_answer_in_show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User can write the answer on the page showing the question', %q{
+  In the order to give help
+  as an user 
+  I'd like to be able to write answer
+} do 
+  given(:question) { create(:question) }
+
+  scenario 'User can write the answer on the page showing question' do
+    visit question_path(question)
+
+    expect(page).to have_field('Your answer:', type: 'textarea')
+    expect(page).to have_button('Save answer')
+  end
+end
