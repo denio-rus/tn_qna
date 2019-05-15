b35c291d35b317e24ceaf28c15e03d318b20441c
denio-rus <denio@yandex.ru>
2019-05-16 01:26:28 +0700

    Add multiple files to question on create

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Question, type: :model do
     expect(question.best_answer).to eq question.answers.order(id: :desc)[3]
   end
 
-  it 'have one attached file' do
-    expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One)
+  it 'have many attached files' do
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
 end
