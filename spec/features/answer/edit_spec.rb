918e24bd42653ca22472de79485965a7da6ea980
denio-rus <denio@yandex.ru>
2019-05-16 14:33:40 +0700

    Add file attachments to answer on edit

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -20,35 +20,44 @@ feature 'User can edit his answers', %q{
   describe 'Authenticated user', js: true do
     background { sign_in user }
 
-    scenario 'edits his answer'do 
-      visit question_path(question)
-
-      click_on 'Edit'
-
-      within '.answers' do
-        fill_in "Your answer:", with: 'edited answer'
-        click_on 'Save'
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
-      end
-    end
-
-    scenario 'tries to edit his answer with errors' do
-      visit question_path(question)
-
-      click_on 'Edit'
-
-      within '.answers' do
-        fill_in "Your answer:", with: ''
-        click_on 'Save'
+    describe 'As an author' do
+      background do
+        visit question_path(question)
+        click_on 'Edit'
       end
 
-      expect(page).to have_content answer.body
-      expect(page).to have_content "Body can't be blank"
-    end
+      scenario 'edits his answer'do 
+        within '.answers' do
+          fill_in "Your answer:", with: 'edited answer'
+          click_on 'Save'
 
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
+
+      scenario 'attaches files to his answer on edit' do
+        within '.answers' do
+          attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on 'Save'
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
+      end
+
+      scenario 'tries to edit his answer with errors' do
+        within '.answers' do
+          fill_in "Your answer:", with: ''
+          click_on 'Save'
+        end
+
+        expect(page).to have_content answer.body
+        expect(page).to have_content "Body can't be blank"
+      end
+    end
+    
     scenario "tries to edit other user's answer" do
       question = create(:question_with_answers)
       visit question_path(question)
