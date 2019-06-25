d579fdc4c0e544538c6b5108a6a0d29f1e8be481
denio-rus <denio@yandex.ru>
2019-06-25 13:51:19 +0700

    Add CanCanCan authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def set_user_id_for_js
     gon.user_id = current_user.id if current_user
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+  
+  check_authorization
 end
