f5abced9096d8f15911f475d79a15545d711026d
denio-rus <denio@yandex.ru>
2019-06-28 22:27:20 +0700

    Add API profiles/me

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   use_doorkeeper
   root to: 'questions#index'
-  
+
   devise_for :users, controllers: { omniauth_callbacks: 'oauth_callbacks', registrations: 'user/registrations' }
   devise_scope :user do
     patch 'users/:id/ask_email_for_oauth/', to: 'user/registrations#ask_email_for_oauth', as: 'user'
@@ -31,4 +31,12 @@ Rails.application.routes.draw do
 
   default_url_options host: 'localhost:3000'
   mount ActionCable.server => '/cable'
+
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: [] do
+        get :me, on: :collection
+      end
+    end
+  end
 end
