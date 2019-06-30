29ef3daa896616ad99f9ed9ca2e0442d170b6028
denio-rus <denio@yandex.ru>
2019-06-30 17:33:19 +0700

    Add API question - CUD, answer - CUD

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::BaseController < ApplicationController
   skip_authorization_check
   before_action :doorkeeper_authorize!
+
+  protect_from_forgery with: :null_session
   
   private 
+  
+  def current_user 
+    current_resource_owner
+  end
 
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
