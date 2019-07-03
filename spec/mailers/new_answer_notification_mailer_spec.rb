1feb3c638ac3d0e76b9934e0bee388f30b9561d3
denio-rus <denio@yandex.ru>
2019-07-04 00:15:01 +0700

    Green tests

--- spec/mailers/new_answer_notification_mailer_spec.rb
+++ spec/mailers/new_answer_notification_mailer_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 RSpec.describe NewAnswerNotificationMailer, type: :mailer do
   describe "notice" do
     let(:answer) { create(:answer) }
-    let(:mail) { NewAnswerNotificationMailer.notice(answer) }
+    let(:subscribe) { create(:subscribe, question: answer.question) }
+    let(:mail) { NewAnswerNotificationMailer.notice(subscribe.user, answer) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Notice")
-      expect(mail.to).to eq([answer.question.author.email])
+      expect(mail.to).to eq([subscribe.user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
--- spec/models/subscribe_spec.rb
+++ spec/models/subscribe_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Subscribe, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:question) }
    
-  #it { should validate_uniqueness_of(:user_id).scoped_to(:question_id) }
   it 'validates uniqueness of combination of user_id and question_id' do
     question = create(:question)
     user = create(:user)
