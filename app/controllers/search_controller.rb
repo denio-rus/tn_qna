dd980e55cb9294e5806552081d43f604fc3ca6b0
denio-rus <denio@yandex.ru>
2019-07-06 03:56:34 +0700

    Add SearchControler#result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,29 @@ class SearchController < ApplicationController
   skip_authorization_check
 
   def result
-    Services::Search.call(params[:query], params[:query_object])
+    if params[:query].present?
+      parse_result(Services::Search.call(params[:query], params[:query_object]))
+    else
+      redirect_to root_path, alert: 'Empty query'
+    end
+  end
+
+  private
+
+  def parse_result(result)
+    @questions, @answers, @comments, @people = [], [], [], []
+
+    result.each do |item|
+      case item.class.name
+      when "Question"
+        @questions << item
+      when "Answer"
+        @answers << item
+      when "Comment"
+        @comments << item
+      else
+        @people << item
+      end
+    end
   end
 end
