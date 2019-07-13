d22a69007e5f990894148b3dddacbfdd717d845e
denio-rus <denio@yandex.ru>
2019-07-13 10:34:47 +0700

    Add cache  with redis

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates :body, presence: true
 end
